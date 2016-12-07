6606287ea055bb7e104fada94680b0e4c9250176
Jubayer Nil <jubayernil@Macbooks-MacBook-Pro-2.local>
2016-12-07 11:52:46 +0600

    Update main file

--- app/src/main/java/com/example/jubayernil/gittest/MainActivity.java
+++ app/src/main/java/com/example/jubayernil/gittest/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.jubayernil.gittest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.print("nil");
+        Log.d("Nice", "onCreate: "+"Hello");
     }
 }
